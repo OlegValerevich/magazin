84574e09f1bc5599f9b611077787399da1ae115d
OlegValerevich <oleg.valerevich85@gmail.com>
2019-04-19 19:38:49 +0700

    menuWidget

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,58 +22,65 @@ AppAsset::register($this);
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <?php $this->registerCssFile('css/ie6.css', ['position' => \yii\web\View::POS_HEAD, 'condition' => 'lte IE6']);  ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
+    <!-- Shell -->	
+<div class="shell">
+	
+	<!-- Header -->	
+	<div id="header">
+		<h1 id="logo"><a href="#">shoparound</a></h1>	
+		
+		<!-- Cart -->
+		<div id="cart">
+			<a href="#" class="cart-link">Your Shopping Cart</a>
+			<div class="cl">&nbsp;</div>
+			<span>Articles: <strong>4</strong></span>
+			&nbsp;&nbsp;
+			<span>Cost: <strong>$250.99</strong></span>
+		</div>
+		<!-- End Cart -->
+		
+		<!-- Navigation -->
+		<div id="navigation">
+			<ul>
+			    <li><a href="#" class="active">Home</a></li>
+			    <li><a href="#">Support</a></li>
+			    <li><a href="#">My Account</a></li>
+			    <li><a href="#">The Store</a></li>
+			    <li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+		<!-- End Navigation -->
+	</div>
+	<!-- End Header -->
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
         <?= $content ?>
-    </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+        
+        <!-- Footer -->
+	<div id="footer">
+		<p class="left">
+			<a href="#">Home</a>
+			<span>|</span>
+			<a href="#">Support</a>
+			<span>|</span>
+			<a href="#">My Account</a>
+			<span>|</span>
+			<a href="#">The Store</a>
+			<span>|</span>
+			<a href="#">Contact</a>
+		</p>
+		<p class="right">
+			&copy; 2010 Shop Around.
+			Design by <a href="http://chocotemplates.com" target="_blank" title="The Sweetest CSS Templates WorldWide">Chocotemplates.com</a>
+		</p>
+	</div>
+	<!-- End Footer -->
+	
+</div>	
+<!-- End Shell -->
 
 <?php $this->endBody() ?>
 </body>
